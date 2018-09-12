314b0fdca0faac61ee3fbe52faf6f410f99b641e
dodevs <douglass.sousa@outlook.com.br>
2018-09-12 14:41:52 +0000

    Exercicio 5 iniciado

---
+++ exercicio_5.c
@@ -0,0 +1,23 @@
+/* Escreva um algoritmo que lê do teclado uma matriz 4x2 de inteiros. A seguir, troque os elementos da
+ * primeira linha com os elementos da segunda linha e os da terceira linha com a quarta linha.
+ * Imprima a matriz resultante.
+ */
+
+#include <stdio.h>
+
+void move_linhas(int mat[][2], int saida, int destino){
+    int *temp;
+    temp = 
+}
+
+int main(){
+    int matriz[4][2];
+
+    for(int i=0; i < 4; i++){
+        for(int j=0; i< 2; i++){
+            scanf("%d", &mat[i][j]);
+        }
+    }
+    return 0;
+}
+
