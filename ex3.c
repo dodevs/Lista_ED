9abc58fc736d0bd445afc0c3f65cc2c27c0e8c29
dodevs <douglass.sousa@outlook.com.br>
2018-09-13 22:44:31 -0300

    Funciona dinamicamente

--- ex3.c
+++ ex3.c
@@ -1,10 +1,9 @@
-/* Faca um programa que le um vetor de inteiros e remove os elementos contendo o 
+/* Faca um programa que le um vetor de inteiros e remove os elementos contendo o
  * valor 0 (ransfira-os para o final do vetor). Mostre o  vetor resultante na tela.
- * Exemplo: Ovetor 0 1 3 -1 0 0 5 fica 1 3 -1 5 0 0 
+ * Exemplo: Ovetor 0 1 3 -1 0 0 5 fica 1 3 -1 5 0 0
  */
 
 #include <stdio.h>
-#define VET_TAM 10
 
 void shift(int *v, int tam, int idx){
 	for(; idx < tam; idx++)
@@ -12,7 +11,9 @@ void shift(int *v, int tam, int idx){
 }
 
 int main(){
-	int vetorOriginal[VET_TAM] = {0, 1, 3, -1, 0, 0, 5, 0, 4, 0}; /* Resultado final deve ser {1, 3, -1, 5, 4, 0, 0, 0, 0, 0} */
+	int vetorOriginal[] = {0,1,1,0,0}; /* Resultado final deve ser {1, 3, -1, 5, 4, 0, 0, 0, 0, 0} */
+	int VET_TAM = sizeof(vetorOriginal)/sizeof(vetorOriginal[0]);
+
 	int i = 0, z = 0;
 	while(i != VET_TAM){
 		if(i == VET_TAM - z)
