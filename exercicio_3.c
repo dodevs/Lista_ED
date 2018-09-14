d6c9b27ec1c474bdd90a85ee8e68748a985c8c7a
IAmTheEnd <matheus_garcias@hotmail.com>
2018-09-14 19:29:03 -0300

    Resolvendo problema

--- exercicio_3.c
+++ exercicio_3.c
@@ -7,19 +7,23 @@
 #define VET_TAM 10
 
 void shift(int *v, int tam, int idx){
-	for(; idx < tam; idx++){
+	for(; idx < tam; idx++)
 		v[idx] = v[idx+1];
-	}
 }
 
 int main(){
 	int vetorOriginal[VET_TAM] = {0, 1, 3, -1, 0, 0, 5, 0, 4, 0}; /* Resultado final deve ser {1, 3, -1, 5, 4, 0, 0, 0, 0, 0} */
-	//shift(vetorOriginal, VET_TAM, 3);
-	for(int i = 0; i < VET_TAM; i++){
+	int i = 0, z = 0;
+	while(i != VET_TAM){
+		if(i == VET_TAM - z)
+			break;
 		if(vetorOriginal[i] == 0){
 			shift(vetorOriginal, VET_TAM, i);
 			vetorOriginal[VET_TAM-1] = 0;
+			z++;
+			continue;
 		}
+		i++;
 	}
 	for(int i = 0; i < VET_TAM; i++){
 		printf("%d ", vetorOriginal[i]);
